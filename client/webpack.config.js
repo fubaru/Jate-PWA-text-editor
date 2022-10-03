80ccba014f448f6bcef8399cfe69e00db2a2ea37
fuaus <kotingfunk@gmail.com>
2022-10-03 15:16:42 -0400

    complete webpack config todo list

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // generated html file and injects bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Another Text Editor',
+        short_name: 'Jate',
+        description: 'It is just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
